3ebd509c1ea423dd92db55694c057f47c5923220
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-05-04 21:13:32 +0530

    err slv 1

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"), //webDir
+    outDir: path.resolve(import.meta.dirname, "dist", "public"), //webDir
     emptyOutDir: true,
   },
   optimizeDeps: {
