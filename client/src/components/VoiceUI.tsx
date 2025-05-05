75ef1f32b929dd24cc995962e8ae3985271e2fdc
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2025-05-05 21:11:23 +0530

    beta test 2.0

--- client/src/components/VoiceUI.tsx
+++ client/src/components/VoiceUI.tsx
@@ -10,6 +10,9 @@ interface VoiceUIProps {
   isInferring: boolean;
   onRecordToggle: () => void;
   voicePreference?: string; // Add voice preference prop
+  isWakeWordMode?: boolean; // <-- Add this
+  isWakeWordListening?: boolean; // <-- Add this
+  onWakeWordDetected?: () => void; // <-- Add this
 }
 
 export const VoiceUI: FC<VoiceUIProps> = ({
@@ -386,18 +389,31 @@ export const VoiceUI: FC<VoiceUIProps> = ({
           flex items-center justify-center hover:scale-110 transition-transform duration-300"
         whileTap={{ scale: 0.9 }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-8 w-8"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z"
-            clipRule="evenodd"
-          />
-        </svg>
+        {isListening ? (
+          // Stop icon when listening
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <rect x="5" y="5" width="10" height="10" rx="2" />
+          </svg>
+        ) : (
+          // Mic icon when not listening
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
       </motion.button>
     </motion.div>
   );
